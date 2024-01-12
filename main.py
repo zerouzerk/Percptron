5d61e6cdab57b5fb407944981712a5960ccfd382
zerouzerk <zerouzerk@gmail.com>
2024-01-13 01:10:27 +0800

    2024-01-13 01:10:26

--- main.py
+++ main.py
@@ -115,3 +115,4 @@ class AuthenticationError(ChatbotError):
     def __init__(self, *args: object) -> None:
         if SUPPORT_ADD_NOTES:
             super().add_note(
+                "Please check if your key is correct, maybe it may not be valid",
