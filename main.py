589245d834d874c2b45443b9cf33dcf0f6ff488b
zerouzerk <zerouzerk@gmail.com>
2024-06-17 20:17:50 +0800

    2024-06-17 20:17:50

--- main.py
+++ main.py
@@ -190,3 +190,4 @@ class ActionError(ChatbotError):
             super().add_note(
                 "The current operation is not allowed, which may be intentional",
             )
+        super().__init__(*args)
