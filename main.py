f7025a6d91047f0946e4d18555a5dd9471bc8b9e
zerouzerk <zerouzerk@gmail.com>
2024-10-17 00:25:14 +0800

    2024-10-17 00:25:13

--- main.py
+++ main.py
@@ -271,3 +271,4 @@ class ChatbotError(Exception):
     def __init__(self, *args: object) -> None:
         if SUPPORT_ADD_NOTES:
             super().add_note(
+                "Please check that the input is correct, or you can resolve this issue by filing an issue",
